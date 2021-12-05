70153cb85a624c4e8b6a45af9d5f3335ccdfb97c
Sean Craig <seancraig21@gmail.com>
2021-12-05 12:21:06 -0600

    finsihed resolvers and started on the client side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,63 @@ const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  Query: {},
-  Mutation: {}
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findById({ _id: context.user.id }).populate("books");
+      }
+      throw new AuthenticationError("You need to be logged in.");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, args, { email, password }) => {
+      const user = await User.findOne({ email });
 
+      if (!user) {
+        throw new AuthenticationError("No user found with the given email");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args.input } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("You need to be logged in");
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("You need to be logged in");
+    },
+  },
 };
 
 export default resolvers;
