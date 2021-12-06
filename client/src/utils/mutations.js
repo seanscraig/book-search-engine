c3802aa569f56de4f4b7d3360f3a0c03ee35b54e
Sean Craig <seancraig21@gmail.com>
2021-12-06 16:22:30 -0600

    finished queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,61 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gpl`
+  mutation login($email: String!, $password:String!){
+    login(email: $email, password: $password) {
+      token
+      user{
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gpl`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password){
+      token
+      user{
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gpl`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      saveBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gpl`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      saveBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
